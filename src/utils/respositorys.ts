d9f4a31f29a0a15cf45f3bd1631e3641f09956e8
Bruno Pavanelli <brunopavanellicontato@gmail.com>
2023-07-19 15:12:59 -0300

    feat: add repositories object

---
+++ src/utils/respositorys.ts
@@ -0,0 +1,19 @@
+import { AppDataSource } from "../data-source";
+
+import { Repository } from "typeorm";
+import { Like, Post, User, Comment } from "../entities";
+
+
+const user: Repository<User> = AppDataSource.getRepository(User);
+const post: Repository<Post> = AppDataSource.getRepository(Post);
+const comment: Repository<Comment> = AppDataSource.getRepository(Comment);
+const like: Repository<Like> = AppDataSource.getRepository(Like);
+
+const repositories = {
+    user,
+    post,
+    comment,
+    like
+};
+
+export default repositories;
