5c3213bf21b5e9869024a8c6788455ac5b0d1264
Bruno Pavanelli <brunopavanellicontato@gmail.com>
2023-07-24 15:54:23 -0300

    fix: remove copy folder

--- src/entities copy/index.ts
+++
@@ -1,6 +0,0 @@
-import User from "./users.entities";
-import Post from "./posts.entities";
-import Comment from "./comments.entities";
-import Like from "./likes.entities";
-
-export { User, Like, Post, Comment };
