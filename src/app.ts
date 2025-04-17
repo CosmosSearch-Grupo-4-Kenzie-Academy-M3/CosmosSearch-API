f2a4e50ca8863898d21f5d7406619703315a249e
jogadorhungria <joilsonhungrianv@gmail.com>
2025-04-17 18:35:00 -0300

    alter rout in login

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,16 @@
 import express, { json } from 'express';
 import "reflect-metadata"
 import "express-async-errors"
-
+import cors from 'cors';
 import routes from './routers';
 import middlewares from './middlewares';
 
 const app = express()
+
+
+
+app.use(cors())
+
 app.use(json())
 
 app.use("/users", routes.users)
